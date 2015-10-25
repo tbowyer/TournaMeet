afc96aba2746bf82153b1e21490f511932d1619d
Trevor Bowyer <bowyer@gmail.com>
2015-10-25 02:55:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tournameet::Application.config.secret_key_base = 'c6fada0b9d7851ace704969304428a189a6e70d6dd94f8eb646db8631df989bc5498263a542e8923e5dc3b53185dd5c8e58c96da7943a395b16471a8d1bc7cdb'
