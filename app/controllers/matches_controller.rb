4dc7aa25504b40c442fe9918d42a7746c53415c5
Trevor Bowyer <bowyer@gmail.com>
2016-12-29 23:36:25 -0500

    Got match reporting to actually move the winner into the next match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -11,15 +11,15 @@ class MatchesController < ApplicationController
 		@next_match = Match.find(@match.next_match_id)
 		#figure out way to pass in the winners id here
 		
+		#if the current match is child 1 of the next match, move the winn into position 2. 
+		#if the current match is child 2, move the winner into position 1
 		@match.winner = @match.player1_id
 		if Match.find(@next_match.child_one_id) == @match
 			@next_match.update_attributes(:player2_id => @match.winner)
-			@match.update_attributes(:player1_id => nil)
-			redirect_to match.id, [@tournament, match]
+			redirect_to tournament_match_path(@next_match.id)
 		elsif Match.find(@next_match.child_two_id) == @match
 			@next_match.update_attributes(:player1_id => @match.winner)
-			@match.update_attributes(:player1_id => nil)
-			redirect_to match.id, [@tournament, match]
+			redirect_to tournament_match_path(@next_match.id)
 		end
 	end
 
